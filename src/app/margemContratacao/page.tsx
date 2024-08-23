659215e1b776185c149edcc1910353806295a77b
Anderson Pereira da Silva <92325762+andersonpdsoficial@users.noreply.github.com>
2024-08-23 07:54:02 -0400

    Update page.tsx
    MODIFICANDO SOMENTE O SOL

--- src/app/margemContratacao/page.tsx
+++ src/app/margemContratacao/page.tsx
@@ -39,8 +39,8 @@ const MargemContratacao = () => {
     setError(null);
   };
 
-  // Customização de datas
-  const CustomDateCalendar = styled(DateCalendar)({
+   // Customização de datas
+   const CustomDateCalendar = styled(DateCalendar)({
     '& .MuiDayCalendar-weekContainer, & .MuiDayCalendar-header, & .MuiDayCalendar-slideTransition, & .MuiDayCalendar-monthContainer': {
       display: 'none',
     },
@@ -53,7 +53,7 @@ const MargemContratacao = () => {
     alignItems: 'center',
   });
 
-  function CustomCalendarHeaderMonth(props: PickersCalendarHeaderProps<Dayjs>) {
+   function CustomCalendarHeaderMonth(props: PickersCalendarHeaderProps<Dayjs>) {
     const { currentMonth, onMonthChange } = props;
 
     const selectNextMonth = () => onMonthChange(currentMonth.add(1, 'month'), 'left');
@@ -187,6 +187,7 @@ const MargemContratacao = () => {
                   </Grid>
                 </Grid>
 
+
                 <Grid container spacing={2} marginTop={2}>
                   <Grid item xs={12} sm={3}>
                     <Typography variant="body2">
@@ -209,6 +210,9 @@ const MargemContratacao = () => {
                   <Divider />
                 </Box>
 
+                {/*  O PREENCHIEMNTO AUTOMATICO FOI ATE ESSE PONTO */}
+
+                
                 <Grid container spacing={2} marginTop={2} alignItems="center">
                   <Grid item xs={12} sm={6}>
                     <Typography variant="body2" color="primary">
@@ -218,7 +222,7 @@ const MargemContratacao = () => {
                 </Grid>
 
                 <Grid container spacing={2} marginTop={2} alignItems="center">
-                  <Grid item xs={12} sm={7}>
+                  <Grid item xs={12} sm={7}> {/*verificar*/}
                     <Grid container spacing={1}>
                       <Grid item xs={6}>
                         <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="pt-br">
@@ -244,6 +248,96 @@ const MargemContratacao = () => {
                         </LocalizationProvider>
                       </Grid>
                     </Grid>
+                    <Box sx={{ backgroundColor: '#e3f2fd', padding: '7px', borderRadius: '8px', textAlign: 'center', marginTop: '10px' }}>
+                      <Typography variant="h6">Margem Total</Typography>
+                      <TextField
+                        label="Valor da Margem"
+                        value={externalData.margem || ''}
+                        onChange={(e) => setCpf(e.target.value)}
+                        fullWidth
+                        variant="outlined"
+                      />
+                    </Box>
+                  </Grid>
+                  <Grid item xs={12} sm={5} display="flex" flexDirection="column" alignItems="center">
+                    <Button variant="contained" color="primary" fullWidth sx={{ backgroundColor: '#0D7B52' }}>
+                      Calcular Margem
+                    </Button>
+                    <Box sx={{ backgroundColor: '#e8f5e9', padding: '20px', borderRadius: '8px', textAlign: 'center', marginTop: '10px', width: '100%' }}>
+                      <Typography variant="h6">Margem Disponível</Typography>
+                      <Typography variant="h5" color="primary">R$-</Typography>
+                    </Box>
+                  </Grid>
+                </Grid>
+
+                <Box marginTop={2}>
+                  <Divider />
+                </Box>
+
+                <Grid container spacing={2} marginTop={2} alignItems="center">
+                  <Grid item xs={12} sm={3}>
+                    <Typography variant="body2">
+                      <strong>Empréstimo:</strong> {/* Valor do Empréstimo */}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12} sm={3}>
+                    <Typography variant="body2">
+                      <strong>Refinanciamento:</strong> {/* Valor do Refinanciamento */}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12} sm={3}>
+                    <Typography variant="body2">
+                      <strong>Composição:</strong> {/* Valor da Composição */}
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12} sm={3}>
+                    <Typography variant="body2">
+                      <strong>Portabilidade:</strong> {/* Valor da Portabilidade */}
+                    </Typography>
+                  </Grid>
+                </Grid>
+
+                <Box marginTop={2}>
+                  <Divider />
+                </Box>
+
+                <Grid container spacing={2} marginTop={2} alignItems="center">
+                  <Grid item xs={12}>
+                    <Typography variant="h6" gutterBottom>
+                      Contratos
+                    </Typography>
+                    <Grid container spacing={2}>
+                      <Grid item xs={2}>
+                        <Typography variant="body2">
+                          <strong>Situação:</strong> {/* Situação do Contrato */}
+                        </Typography>
+                      </Grid>
+                      <Grid item xs={2}>
+                        <Typography variant="body2">
+                          <strong>BOT:</strong> {/* BOT */}
+                        </Typography>
+                      </Grid>
+                      <Grid item xs={2}>
+                        <Typography variant="body2">
+                          <strong>Data de Contratação:</strong> {/* Data de Contratação */}
+                        </Typography>
+                      </Grid>
+                      <Grid item xs={2}>
+                        <Typography variant="body2">
+                          <strong>Total Parcelas:</strong> {/* Total de Parcelas */}
+                        </Typography>
+                      </Grid>
+                      <Grid item xs={2}>
+                        <Typography variant="body2">
+                          <strong>Valor Parcela:</strong> {/* Valor da Parcela */}
+                        </Typography>
+                      </Grid>
+                      <Grid item xs={2}>
+                        <Typography variant="body2">
+                          <strong>Crédito:</strong> {/* Valor do Crédito */}
+                        </Typography>
+                      </Grid>
+                    </Grid>
                   </Grid>
                 </Grid>
               </>
@@ -251,13 +345,12 @@ const MargemContratacao = () => {
           </>
         )}
 
-        {isError && fetchError && (
-          <Snackbar open={true} autoHideDuration={6000} onClose={() => setError(null)}>
-            <Alert onClose={() => setError(null)} severity="error">
-              {fetchError.message}
-            </Alert>
-          </Snackbar>
-        )}
+        <Snackbar open={Boolean(error)} autoHideDuration={6000} onClose={() => setError(null)}>
+          <Alert onClose={() => setError(null)} severity="error">{error}</Alert>
+        </Snackbar>
+        <Snackbar open={Boolean(fetchError)} autoHideDuration={6000} onClose={() => setError(null)}>
+          <Alert onClose={() => setError(null)} severity="error">{fetchError?.message}</Alert>
+        </Snackbar>
       </Box>
     </Box>
   );

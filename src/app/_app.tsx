306ee1b08aa61e8635c8416b05373969be14234f
Anderson Pereira da Silva <92325762+andersonpdsoficial@users.noreply.github.com>
2024-09-29 23:30:42 -0400

    ajustado 29

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,23 +1,33 @@
-'use client'; 
+'use client';
+
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AppProps } from 'next/app';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import CookiesBanner from '../shared/components/cookiesBanner/CookiesBanner';
 import NovoEmprestimo from './margemContratacao/emprestimoAverbacoes/page';
+import { createTheme, ThemeProvider } from '@mui/material';
 
 // Crie uma instância do QueryClient
 const queryClient = new QueryClient();
+const theme = createTheme({
+  palette: {
+    mode: 'light', // ou 'dark'
+  },
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <CssBaseline />
-      <CookiesBanner />
-      <NovoEmprestimo />
-      <Component {...pageProps} />
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <ThemeProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <CssBaseline />
+        <CookiesBanner />
+        {/* Renderiza o NovoEmprestimo apenas em uma condição, se necessário */}
+        <NovoEmprestimo />
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
 

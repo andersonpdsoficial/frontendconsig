21a8a22aa7018cf3a388e9f4055154b21dbf2c35
Anderson Pereira da Silva <92325762+andersonpdsoficial@users.noreply.github.com>
2024-09-29 23:26:00 -0400

    ajustado 29

--- src/app/margemContratacao/emprestimoAverbacoes/page.tsx
+++ src/app/margemContratacao/emprestimoAverbacoes/page.tsx
@@ -1,11 +1,11 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useForm, Controller } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
 import {
-  Grid,  Button,  TextField,  Typography,  Stepper,  Step,  StepLabel,  Box,
+  Grid, Button, TextField, Typography, Stepper, Step, StepLabel, Box,
 } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
 import { useReserva } from '../../../shared/hooks/useReserva';
@@ -13,11 +13,13 @@ import { fetchConsultas } from '../../../shared/services/apiService';
 import CustomizedList from '../../../shared/components/menu-lateral/Demo';
 import FloatingSearchButton from '../../../shared/components/buttons/FloatingSearchButton';
 import CookiesBanner from '../../../shared/components/cookiesBanner/CookiesBanner';
-import { useDadosEmprestimo } from '../__hooks/use-dados-emprestimo';
+import { create } from 'zustand';
+import { PlaylistAddCheckCircle } from '@mui/icons-material';
 
 // Define the validation schema with zod
 const schema = z.object({
   matricula: z.string().min(1, 'Matrícula é obrigatória'),
+  nome: z.string().min(1, 'Nome é obrigatório'),
   cpf: z.string().min(1, 'CPF é obrigatório'),
   valor: z.string().min(1, 'Valor é obrigatório'),
   consulta: z.string().min(1, 'Consulta é obrigatória'),
@@ -43,31 +45,33 @@ const schema = z.object({
 
 const steps = ['Preenchimento dos Dados', 'Análise dos Dados', 'Geração do Contrato'];
 
-const { preencherDados } = useDadosEmprestimo();
-  
-  // Supondo que você tenha os dados de margemContratacao disponíveis
-  const margemContratacao = {
-    cpf: '12345678900',
-    matricula: '123456',
-    margemTotal: 10000,
-    margemDisponivel: 5000,
-  };
+// Defina a interface do estado
+interface Store {
+  cpf: string;
+  matricula: string | null;
+  nome: string;
+  vinculo: string;
+  margemDisponivel: number | null;
+}
 
-  // Quando precisar preencher os dados
-  React.useEffect(() => {
-    preencherDados(margemContratacao);
-  }, [margemContratacao, preencherDados]);
+const useStore = create<Store>((set) => ({
+  cpf: '',
+  nome: '',
+  matricula: null,
+  vinculo: '',
+  margemDisponivel: null,
+  salvarDados: (dados) => set((state) => ({ ...state, ...dados })),
+}));
 
-  const matricula = useDadosEmprestimo((state) => state.matricula);
-  console.log({ matricula });
+const ReservaPage = () => {
+  const { cpf, matricula, nome, vinculo, margemDisponivel } = useStore();
 
-  const { control, handleSubmit, setValue, formState: { errors } } = useForm({
+  const { control, handleSubmit, setValue, formState: { errors }, reset, getValues } = useForm({
     resolver: zodResolver(schema),
   });
 
-  const [activeStep, setActiveStep] = React.useState(0);
-  const { mutate: createReserva, isLoading: isSaving } = useReserva();
-
+  const [activeStep, setActiveStep] = useState(0);
+  const { mutate: createReserva } = useReserva();
   const { data: consultas, isLoading: isLoadingConsultas, isError: isErrorConsultas } = useQuery({
     queryKey: ['consultas'],
     queryFn: fetchConsultas,
@@ -80,29 +84,43 @@ const { preencherDados } = useDadosEmprestimo();
     }
   }, [consultas, setValue]);
 
+  // Preencher automaticamente os campos do formulário com os dados da store
+  useEffect(() => {
+    setValue('cpf', String(cpf));
+    setValue('matricula', String(matricula)); // Converter para string se necessário
+    setValue('nome', String(nome));
+    setValue('vinculo', String(vinculo));
+    setValue('valor', String(margemDisponivel)); // Converter para string se necessário
+  }, [cpf, matricula, nome, vinculo, margemDisponivel, setValue]);
 
   const onSubmit = (data) => {
     if (activeStep === 1) {
       createReserva(data, {
-        onSuccess: () => {
-          setActiveStep((prev) => prev + 1);
+        onSuccess: () => setActiveStep((prev) => prev + 1),
+        onError: (error) => {
+          console.error('Erro ao criar reserva:', error);
         },
       });
     } else {
       setActiveStep((prev) => prev + 1);
     }
   };
-
   const handleBack = () => {
     setActiveStep((prev) => prev - 1);
   };
 
+  const handleReset = () => {
+    reset();
+    setActiveStep(0);
+  };
+
   return (
+
     <Box sx={{ display: 'flex', backgroundColor: '#F2F2F2', minHeight: '100vh' }}>
       <CustomizedList />
       <FloatingSearchButton />
       <CookiesBanner />
-      <Box sx={{ flexGrow: 2, backgroundColor: '#F2F2F2', padding: 4 }}>
+      <Box sx={{ flexGrow: 2, backgroundColor: '#F2F2F2', padding: 10 }}>
         <Typography variant="h6" sx={{ fontWeight: 'bold', mb: 2 }}>
           Nova Reserva de Empréstimo
         </Typography>
@@ -115,17 +133,124 @@ const { preencherDados } = useDadosEmprestimo();
         </Stepper>
         <Box sx={{ padding: 2 }}>
           {activeStep === 0 && (
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <Grid container spacing={3}>
+            <Box
+              sx={{
+                bgcolor: '#ffff',
+                borderRadius: 2,
+                boxShadow: 3,
+                p: 3,
+                mt: 2,
+              }}
+            >
+              <form onSubmit={handleSubmit(onSubmit)}>
+                <Grid container spacing={3}>
+                  {[
+                    { name: 'matricula', label: 'Matrícula' },
+                    { name: 'cpf', label: 'CPF' },
+                    { name: 'nome', label: 'Nome' },
+                    { name: 'valor', label: 'Valor' },
+                    { name: 'prazo_inicial', label: 'Prazo Inicial', type: 'datetime-local' },
+                    { name: 'prazo_final', label: 'Prazo Final', type: 'datetime-local' },
+                    { name: 'situacao', label: 'Situação' },
+                    { name: 'contrato', label: 'Contrato' },
+                    { name: 'vencimento_parcela', label: 'Vencimento das Parcelas', type: 'date' },
+                    { name: 'folha_desconto', label: 'Folha de Desconto' },
+                    { name: 'total_financiado', label: 'Total Financiado' },
+                    { name: 'liquido_liberado', label: 'Líquido Liberado' },
+                    { name: 'liberacao_credito', label: 'Liberação de Crédito' },
+                    { name: 'cet', label: 'CET' },
+                    { name: 'observacoes', label: 'Observações' },
+                    { name: 'quantidade_parcelas', label: 'Quantidade de Parcelas' },
+                    { name: 'valor_parcelas', label: 'Valor das Parcelas' },
+                    { name: 'juros_mensal', label: 'Juros Mensal' },
+                    { name: 'valor_iof', label: 'Valor do IOF' },
+                    { name: 'carencia_dias', label: 'Dias de Carência' },
+                    { name: 'valor_carencia', label: 'Valor da Carência' },
+                    { name: 'vinculo', label: 'Vínculo' },
+                  ].map(({ name, label, type }) => (
+                    <Grid item xs={12} sm={6} key={name}>
+                      <Controller
+                        name={name}
+                        control={control}
+                        render={({ field }) => (
+                          <TextField
+                            {...field}
+                            label={label}
+                            type={type || 'text'}
+                            fullWidth
+                            required
+                            error={Boolean(errors[name])}
+                            helperText={errors[name]?.message}
+                            InputLabelProps={type === 'datetime-local' || type === 'date' ? { shrink: true } : {}}
+                            variant="outlined"
+                            sx={{ bgcolor: 'white', borderRadius: 1 }}
+                          />
+                        )}
+                      />
+                    </Grid>
+                  ))}
+                  <Grid item xs={12} sm={6}>
+                    <Controller
+                      name="consulta"
+                      control={control}
+                      render={({ field }) => (
+                        <TextField
+                          {...field}
+                          label="Consulta"
+                          fullWidth
+                          required
+                          error={Boolean(errors.consulta)}
+                          helperText={errors.consulta?.message}
+                          InputProps={{ readOnly: true }}
+                          variant="outlined"
+                          sx={{ bgcolor: 'white', borderRadius: 1 }}
+                          InputLabelProps={{
+                            shrink: true, // Faz o rótulo ficar acima do campo
+                          }}
+                        />
+                      )}
+                    />
+                  </Grid>
+
+
+
+                  <Grid item xs={12}>
+                    <Button type="submit" variant="contained" color="success" sx={{ mt: 2 }}>
+                      Próximo
+                    </Button>
+                  </Grid>
+                </Grid>
+              </form>
+            </Box>
+          )}
+
+          {activeStep === 1 && (
+            <Box
+              sx={{
+                width: '90%',
+                maxWidth: '800px',
+                margin: 'auto',
+                padding: 3,
+                boxShadow: 3,
+                borderRadius: 2,
+                backgroundColor: 'success.main',
+                color: 'white',
+              }}
+            >
+              <Typography variant="h6" gutterBottom>
+                Análise dos Dados
+              </Typography>
+              <Grid container spacing={3} sx={{ mt: 2 }}>
                 {[
                   { name: 'matricula', label: 'Matrícula' },
                   { name: 'cpf', label: 'CPF' },
+                  { name: 'nome', label: 'Nome' },
                   { name: 'valor', label: 'Valor' },
-                  { name: 'prazo_inicial', label: 'Prazo Inicial', type: 'datetime-local' },
-                  { name: 'prazo_final', label: 'Prazo Final', type: 'datetime-local' },
+                  { name: 'prazo_inicial', label: 'Prazo Inicial' },
+                  { name: 'prazo_final', label: 'Prazo Final' },
                   { name: 'situacao', label: 'Situação' },
                   { name: 'contrato', label: 'Contrato' },
-                  { name: 'vencimento_parcela', label: 'Vencimento das Parcelas', type: 'date' },
+                  { name: 'vencimento_parcela', label: 'Vencimento das Parcelas' },
                   { name: 'folha_desconto', label: 'Folha de Desconto' },
                   { name: 'total_financiado', label: 'Total Financiado' },
                   { name: 'liquido_liberado', label: 'Líquido Liberado' },
@@ -139,77 +264,63 @@ const { preencherDados } = useDadosEmprestimo();
                   { name: 'carencia_dias', label: 'Dias de Carência' },
                   { name: 'valor_carencia', label: 'Valor da Carência' },
                   { name: 'vinculo', label: 'Vínculo' },
-                ].map(({ name, label, type }) => (
+                ].map(({ name, label }) => (
                   <Grid item xs={12} sm={6} key={name}>
-                    <Controller
-                      name={name}
-                      control={control}
-                      render={({ field }) => (
-                        <TextField
-                          {...field}
-                          label={label}
-                          type={type || 'text'}
-                          fullWidth
-                          required
-                          error={Boolean(errors[name])}
-                          helperText={errors[name]?.message}
-                          InputLabelProps={type === 'datetime-local' || type === 'date' ? { shrink: true } : {}}
-                          variant="outlined"
-                          sx={{ bgcolor: 'white', borderRadius: 1 }}
-                        />
-                      )}
-                    />
+                    <Typography variant="body1">{label}:</Typography>
+                    <Typography variant="body2">{getValues(name)}</Typography>
                   </Grid>
                 ))}
-
-                <Grid item xs={12} sm={6}>
-                  <Controller
-                    name="consulta"
-                    control={control}
-                    render={({ field }) => (
-                      <TextField
-                        {...field}
-                        label="Consulta"
-                        fullWidth
-                        required
-                        error={Boolean(errors.consulta)}
-                        helperText={errors.consulta?.message}
-                        InputProps={{ readOnly: true }}
-                        variant="outlined"
-                        sx={{ bgcolor: 'white', borderRadius: 1 }}
-                      />
-                    )}
-                  />
-                </Grid>
-
-                <Grid item xs={12}>
-                  <Button type="submit" variant="contained" color="success" sx={{ mt: 2 }}>
-                    Próximo
-                  </Button>
-                </Grid>
               </Grid>
-            </form>
-          )}
-
-          {activeStep === 1 && (
-            <Box>
-              <Typography variant="h6">Análise dos Dados</Typography>
               <Button variant="contained" color="secondary" onClick={handleBack} sx={{ mt: 2 }}>
                 Voltar
               </Button>
-              <Button variant="contained" color="success" onClick={handleSubmit(onSubmit)} sx={{ mt: 2 }}>
+              <Button variant="contained" color="lightgreen" onClick={handleSubmit(onSubmit)} sx={{ mt: 2 }}>
                 Confirmar
               </Button>
             </Box>
           )}
 
           {activeStep === 2 && (
-            <Typography variant="h6">Contrato gerado com sucesso!</Typography>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                padding: 3,
+                maxWidth: '600px',
+                margin: 'auto',
+                boxShadow: 3,
+                borderRadius: 2,
+                textAlign: 'center',
+                bgcolor: 'success.main',
+                color: 'white',
+              }}
+            >
+              <PlaylistAddCheckCircle color="inherit" fontSize="large" sx={{ mb: 2 }} />
+              <Typography variant="h6" sx={{ mb: 1 }}>
+                Contrato gerado com sucesso!
+              </Typography>
+              <Typography variant="body1" sx={{ mb: 2 }}>
+                Número do contrato: {getValues('contrato')}
+              </Typography>
+              <Typography variant="body1" sx={{ mb: 2 }}>
+                Status: Enviado para Análise.
+              </Typography>
+              <Button
+                onClick={handleReset}
+                variant="contained"
+                color="lightgreen"
+                sx={{ mt: 2 }}
+              >
+                Finalizar
+              </Button>
+            </Box>
           )}
         </Box>
       </Box>
     </Box>
   );
 };
-
 export default ReservaPage;
+

0a734fd261602ecac2e9d789827241d5e9ad5890
Anderson Pereira da Silva <92325762+andersonpdsoficial@users.noreply.github.com>
2024-09-29 23:36:58 -0400

    ajustado 29

--- src/shared/themes/theme.ts
+++ src/shared/themes/theme.ts
@@ -3,9 +3,8 @@ import { Roboto } from 'next/font/google';
 import { createTheme } from '@mui/material/styles';
 import { cyan, yellow } from '@mui/material/colors';
 
-export  * from './Dark'
-export  * from './Ligth'
-
+export * from './Dark';
+export * from './Light'; // Corrigido o nome da exportação
 
 const roboto = Roboto({
   weight: ['300', '400', '500', '700'],
@@ -33,25 +32,26 @@ const theme = createTheme({
   },
 });
 
-export default theme;
+// Light theme
+export const LightTheme = createTheme({
+  palette: {
+    primary: {
+      main: yellow[700],
+      dark: yellow[800],
+      light: yellow[500],
+      contrastText: '#ffffff',
+    },
+    secondary: {
+      main: cyan[500],
+      dark: cyan[400],
+      light: cyan[300],
+      contrastText: '#ffffff',
+    },
+    background: {
+      default: '#f7f6f3', // Cinza claro no fundo
+      paper: '#ffffff', // Destaque no card
+    },
+  },
+});
 
-export const LightTheme =createTheme( { 
-  palette:{
-      primary:{
-          main: yellow[700] ,
-          dark: yellow[800],
-          light: yellow[500] ,
-          contrastText:' #ffffff' ,
-      },
-      secondary:{
-          main: cyan[500] ,
-          dark: cyan[400],
-          light: cyan[300] ,
-          contrastText:' #ffffff' ,
-      },
-      background:{
-          default: '#f7f6f3', /* Esse vai ser um cinza claro no fundo como de padão pr aplicação */
-          paper: '#ffffff', /* vai ser um destaque no card */
-      }
-  }
-})
+export default theme;

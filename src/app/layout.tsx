ad1e89b54f022b9edaf28f33ce558c0ab5343da8
Anderson Pereira da Silva <92325762+andersonpdsoficial@users.noreply.github.com>
2024-09-29 23:31:12 -0400

    ajustado 29

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-
 import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from '@mui/material/styles';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
